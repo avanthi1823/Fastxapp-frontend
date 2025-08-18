524d58efcaaf513c05e277114176dfc68ce4e960
avanthi1823 <139262237+avanthi1823@users.noreply.github.com>
2025-08-18 21:01:30 +0530

    Add files via upload

---
+++ src/models/register.model.js
@@ -0,0 +1,8 @@
+export class RegisterModel {
+    fullName = "";
+    email = "";
+    password = "";
+    gender = "";
+    phone = "";
+    roleId = 0; // default role ID
+}
---
+++ src/models/registererror.model.js
@@ -0,0 +1,8 @@
+export class RegisterErrorModel {
+    fullName = "";
+    email = "";
+    password = "";
+    gender = "";
+    phone = "";
+    roleId = ""; // usually errors are strings describing the issue
+}
