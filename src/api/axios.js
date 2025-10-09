be74a728ed5f376313f8bdbea972538871ad12c5
avanthi1823 <139262237+avanthi1823@users.noreply.github.com>
2025-10-09 15:43:23 +0530

    Delete src directory

--- src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5151/api",
-});
-
-export default api;
--- src/utils/roles.js
+++
@@ -1,5 +0,0 @@
-export const roles = [
-  { id: 1, name: "Admin" },
-  { id: 2, name: "User" },
-  { id: 3, name: "Operator" },
-];
